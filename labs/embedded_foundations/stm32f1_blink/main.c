422c314c18a8aae907bc0e1527132d49bcb159d1
doruk-d <dorukanakat@gmail.com>
2026-01-24 21:05:03 +0100

    refactor (blinking_led): update toggle logic with BSRR for atomicity

--- labs/embedded_foundations/stm32f1_blink/main.c
+++ labs/embedded_foundations/stm32f1_blink/main.c
@@ -5,6 +5,7 @@
 #define RCC_APB2ENR (*(volatile uint32_t *)(0x40021000 + 0x18))
 #define GPIOA_CRL (*(volatile uint32_t *)(0x40010800 + 0x00))
 #define GPIOA_ODR (*(volatile uint32_t *)(0x40010800 + 0x0C))
+#define GPIOA_BSRR (*(volatile uint32_t *)(0x40010800 + 0x10))
 #define LED 5
 
 extern uint32_t _sidata;
@@ -50,8 +51,15 @@ void main(){
     GPIOA_CRL &= ~(0xF << (LED * 4));
     GPIOA_CRL |= (1 << (LED * 4));
 
+    uint8_t status = 0;
     while (1){
-        GPIOA_ODR ^= (1 << LED);
+        if (status)
+            GPIOA_BSRR = (1 << LED);
+        else
+            GPIOA_BSRR = (1 << (LED + 16));
+
+        status = !status;
+
         delay(1000);
     }
 }
