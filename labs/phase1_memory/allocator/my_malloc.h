d3373726b88f6dec7ef282d40341ef13bb337905
doruk-d <dorukanakat@gmail.com>
2026-02-15 19:51:23 +0100

    feat (allocator): define allocator public API and header guards

--- labs/phase1_memory/allocator/my_malloc.h
+++ labs/phase1_memory/allocator/my_malloc.h
@@ -3,7 +3,9 @@
 
 #include <stddef.h>
 
-void *my_malloc(size_t size);
+void *my_malloc(size_t size);       
+void *my_calloc(size_t nmemb, size_t size);
+void *my_realloc(void *ptr, size_t size);
 void my_free(void *ptr);
 
 #endif
